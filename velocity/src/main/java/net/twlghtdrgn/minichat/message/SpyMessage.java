a3f5fe737be9af2360df5bf64420ca2d14ac9e83
TwlghtDrgn <marakulin6260@gmail.com>
2023-11-06 10:51:46 +0300

    Spying fix

--- velocity/src/main/java/net/twlghtdrgn/minichat/message/SpyMessage.java
+++ velocity/src/main/java/net/twlghtdrgn/minichat/message/SpyMessage.java
@@ -5,73 +5,88 @@ import net.kyori.adventure.text.Component;
 import net.twlghtdrgn.minichat.MiniChat;
 import net.twlghtdrgn.minichat.PlayerCache;
 import net.twlghtdrgn.minichat.ServerCache;
-import net.twlghtdrgn.minichat.config.Configuration;
-import net.twlghtdrgn.minichat.config.Language;
 import net.twlghtdrgn.minichat.event.NetworkMessageEvent;
 import net.twlghtdrgn.twilightlib.api.util.Format;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SpyMessage {
     private final boolean isNetwork;
     private String message;
     private final String sender;
-    private final String value;
-    private final ServerCache.CachedServer cachedServer;
+    private List<String> nicknames = new ArrayList<>();
+    private String serverName = "";
+    private ServerCache.CachedServer cachedServer = new ServerCache.CachedServer(false, "!", false);
 
-    public SpyMessage(boolean isNetwork, String message, String sender, String value) {
+    public SpyMessage(boolean isNetwork, String message, String sender, Object value) {
         this.isNetwork = isNetwork;
         this.message = message;
         this.sender = sender;
-        this.value = value;
-        if (isNetwork && ServerCache.isCached(value))
-            this.cachedServer = ServerCache.getCachedServer(value).get();
-        else
-            this.cachedServer = new ServerCache.CachedServer(false,"!",false);
+        if (value instanceof String serverName) {
+            this.serverName = serverName;
+            if (ServerCache.isCached(serverName)) this.cachedServer = ServerCache.getCachedServer(serverName).get();
+        } else if (value instanceof List<?> recipients) {
+            nicknames = (List<String>) recipients;
+        } else throw new IllegalArgumentException("Value should be a list of players, or server name");
     }
 
     public void send() {
         final List<Player> spies = getPlayers();
 
         String channelType = "";
+        String recipients = "";
         if (isNetwork) {
-            if (cachedServer.isIgnore()) return;
-            if (message.startsWith(cachedServer.getGlobalChatPrefix())) {
-                message = message.replaceFirst(cachedServer.getGlobalChatPrefix(), "");
-                channelType = Configuration.getConfig().getSpy().getGlobalChatSymbol();
-            } else channelType = Configuration.getConfig().getSpy().getLocalChatSymbol();
-        }
+            if (!cachedServer.isLoggingEnabled()) return;
+            if (cachedServer.isChanneled()) {
+                if (message.startsWith(cachedServer.getGlobalChatPrefix())) {
+                    message = message.replaceFirst(cachedServer.getGlobalChatPrefix(), "");
+                    channelType = MiniChat.getPlugin().getConf().get().getSpy().getGlobalChatSymbol();
+                } else {
+                    channelType = MiniChat.getPlugin().getConf().get().getSpy().getLocalChatSymbol();
+                }
+            }
+        } else recipients = String.join(", ", nicknames);
 
         final Component spyMessage = Format.parse(getMessage()
                 .replace("%sender%", sender)
-                .replace("%recipients%", value)
+                .replace("%recipients%", recipients)
                 .replace("%message%", message)
-                .replace("%server%", value)
+                .replace("%server%", serverName)
                 .replace("%channel_type%", channelType));
 
         spies.stream()
                 .filter(player -> player.getCurrentServer().isPresent())
-                .filter(player -> (!isNetwork && Arrays.stream(value.split(",")).noneMatch(s -> s.equalsIgnoreCase(player.getUsername())))
-                        || !player.getCurrentServer().get().getServerInfo().getName().equalsIgnoreCase(value))
                 .filter(player -> !player.getUsername().equalsIgnoreCase(sender))
+                .filter(player -> !isNetwork || !player.getCurrentServer().get().getServerInfo().getName().equalsIgnoreCase(serverName))
+                .filter(player -> !nicknames.contains(player.getUsername()))
                 .forEach(player -> player.sendMessage(spyMessage));
+
+        String consoleChannelType = "";
+        if (isNetwork && cachedServer.isChanneled()) {
+            if (channelType.equals(MiniChat.getPlugin().getConf().get().getSpy().getGlobalChatSymbol())) {
+                consoleChannelType = "G";
+            } else {
+                consoleChannelType = "L";
+            }
+        }
 
         String consoleSpyMessage = getConsoleMessage()
                 .replace("%sender%", sender)
-                .replace("%recipients%", value)
+                .replace("%recipients%", recipients)
                 .replace("%message%", message)
-                .replace("%server%", value)
-                .replace("%channel_type%", channelType.equals(Configuration.getConfig().getSpy().getLocalChatSymbol())
-                        ? Language.getConfig().getSpy().getLocalChatSymbol() : Language.getConfig().getSpy().getGlobalChatSymbol());
+                .replace("%server%", serverName)
+                .replace("%channel_type%", consoleChannelType);
 
         if (isNetwork) {
-            MiniChat.getPlugin().getServer().getEventManager().fire(new NetworkMessageEvent(sender, message, value, channelType));
-            if (Configuration.getConfig().getLogging().isNetworkLoggingEnabled())
+            MiniChat.getPlugin().getServer().getEventManager().fire(new NetworkMessageEvent(sender, message, serverName, consoleChannelType));
+            if (MiniChat.getPlugin().getConf().get().getLogging().isNetworkLoggingEnabled())
                 MiniChat.getPlugin().getLogger().info(consoleSpyMessage);
         } else {
-            if (Configuration.getConfig().getLogging().isDirectMessageLoggingEnabled())
+            if (MiniChat.getPlugin().getConf().get().getLogging().isDirectMessageLoggingEnabled())
                 MiniChat.getPlugin().getLogger().info(consoleSpyMessage);
         }
     }
@@ -82,12 +97,12 @@ public class SpyMessage {
     }
 
     private @NotNull String getMessage() {
-        if (isNetwork) return Language.getConfig().getSpy().getNetworkSpyFormat();
-        else return Language.getConfig().getSpy().getSocialSpyFormat();
+        if (isNetwork) return MiniChat.getPlugin().getLang().get().getSpy().getNetworkSpyFormat();
+        else return MiniChat.getPlugin().getLang().get().getSpy().getSocialSpyFormat();
     }
 
     private @NotNull String getConsoleMessage() {
-        if (isNetwork) return Language.getConfig().getSpy().getNetworkSpyConsoleFormat();
-        else return Language.getConfig().getSpy().getSocialSpyConsoleFormat();
+        if (isNetwork) return MiniChat.getPlugin().getLang().get().getSpy().getNetworkSpyConsoleFormat();
+        else return MiniChat.getPlugin().getLang().get().getSpy().getSocialSpyConsoleFormat();
     }
 }
